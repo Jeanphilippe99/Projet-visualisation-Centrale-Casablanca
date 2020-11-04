dc863e1ef9b694552193e6225611b2d93c69e97e
kouadio jean philippe d'anzi <jko@iknova.com>
2020-11-04 19:37:42 -0100

    amelioration
    add tooltips and colors in map

--- map_got.R
+++ map_got.R
@@ -1,6 +1,12 @@
 library(sf)
 library(ggplot2)
+library(dplyr)
 continents = st_read("data/GoTRelease/Continents.shp", crs=4326)
+episodes = st_read("data/episodes.csv", crs=4326)
+appearances = st_read("data/appearances.csv", crs=4326)
+characters = st_read("data/characters.csv", crs=4326)
+populations = st_read("data/populations.csv", crs=4326)
+scenes = st_read("data/scenes.csv", crs=4326)
 islands = st_read("data/GoTRelease/Islands.shp", crs=4326)
 lakes = st_read("data/GoTRelease/Lakes.shp", crs=4326)
 land = st_read("data/GoTRelease/Land.shp", crs=4326)
@@ -13,5 +19,30 @@ roads = st_read("data/GoTRelease/Roads.shp", crs=4326)
 scenesLocations = st_read("data/GoTRelease/ScenesLocations.shp", crs=4326)
 wall = st_read("data/GoTRelease/Wall.shp",crs=4326)
 
-mtq = continents$geometry
-plot(st_geometry(mtq)) + plot(st_geometry(locations$geometry))
+montains = landscape %>%  filter(type=="montain")
+forests = landscape %>%  filter(type=="forest")
+swamp = landscape %>%  filter(type=="swamp")
+stepp = landscape %>%  filter(type=="stepp")
+
+map = ggplot()+geom_sf(data=continents$geometry,fill="ivory",color="ivory3")
+
+map=ggplot(locations)+geom_sf(data=continents$geometry,fill="ivory",color="ivory3")+
+  geom_sf(data = islands$geometry, fill="ivory",color="ivory3") +
+  geom_sf(data = forests$geometry, fill="green",color="ivory")+   
+  geom_sf(data = montains$geometry, fill="#CCCCCC",color="ivory")+ 
+  geom_sf(data = swamp$geometry, fill="#669999",color="ivory")+ 
+  geom_sf(data = stepp$geometry, fill="#669933",color="ivory")+
+  geom_sf(data = lakes$geometry, fill="#33CCFF",color="black")+ 
+  geom_sf(data = rivers$geometry, fill="#00CCFF",color="blue")+ 
+  geom_sf(data = roads$geometry, fill="#666666",color="#666666")
+
+mapLocation = map +
+  geom_sf(data = locations$geometry, fill="#FFCCCC",color="#FFCCCC")
+
+my_gg <- mapLocation + geom_sf_interactive(aes(tooltip = name), size = 2)
+
+ggiraph(code = print(my_gg))
+
+
+
+
