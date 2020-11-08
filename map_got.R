a207b519c526ce505045136f9d6bc4f6e58f72f9
kouadio jean philippe d'anzi <jko@iknova.com>
2020-11-08 22:02:46 -0100

    test to debug kouman code

---
+++ map_got.R
@@ -0,0 +1,96 @@
+library(sf)
+library(ggplot2)
+library(dplyr)
+library(ggiraph)
+library(roxygen2)
+
+#' import de la base de données
+
+continents = st_read("data/GoTRelease/Continents.shp", crs = 4326)
+islands = st_read("data/GoTRelease/Islands.shp", crs = 4326)
+lakes = st_read("data/GoTRelease/Lakes.shp", crs = 4326)
+landscape = st_read("data/GoTRelease/Landscape.shp", crs = 4326)
+locations = st_read("data/GoTRelease/Locations.shp", crs = 4326)
+rivers = st_read("data/GoTRelease/Rivers.shp", crs = 4326)
+roads = st_read("data/GoTRelease/Roads.shp", crs = 4326)
+wall = st_read("data/GoTRelease/Wall.shp", crs = 4326)
+
+
+#' creation d'une dataframe contenant toutes les datas utiles.
+#' Pour pouvoir reconnaitre les differentes datas, ajout de l'attribut type
+
+continents$type="continent"
+islands$type="island"
+lakes$type="lake"
+rivers$type="river"
+roads$type="road"
+wall$type="wall"
+
+
+allDatas = bind_rows( continents,islands, landscape, rivers,lakes,roads,wall)
+
+spaces = c(
+  "continent",
+  "forest",
+  "mountain",
+  "stepp",
+  "swamp",
+  "lake",
+  "river",
+  "road",
+  "island",
+  "location",
+  "wall",
+  "political",
+  "desert",
+  "land",
+  "shore",
+  "water"
+)
+
+cols = c(
+  "ivory",
+  "green",
+  "gray88",
+  "#669933",
+  "cyan4",
+  "blue",
+  "cyan3",
+  "darkgray",
+  "gold",
+  "black",
+  "gold4",
+  "orangered3",
+  "darkgoldenrod1",
+  "gray",
+  "yellow",
+  "#33CCFF"
+)
+
+
+names(cols) = spaces
+levels(allDatas$type) = spaces
+
+#'\code{displayMap} Consruire la map de got en fonction
+#'@param layerGeometry la couche donc la géometrie sera ajoutée et
+#'@param layerCol pour fixer la couleur de la couche
+#'@param layerFill pour fixer la couleur du remplissage
+#'@return une novelle map
+displayMap = function(layerGeometry=NA, layerCol="red", layerFill="red") {
+  map = ggplot(allDatas) + geom_sf(aes(fill = type), size = 0.1) +
+    geom_sf(data = locations,fill = "black",color = "black") +
+    scale_fill_manual("Lands category", values = cols) +
+    theme_minimal() +
+    geom_sf_interactive(data = locations, aes(tooltip = name), size = 2) +
+    geom_sf_text(
+      data = allDatas %>% filter(type == "continent"),
+      aes(label = name),
+      color = "red",
+      fontface = "bold"
+    )
+  if(!is.na(layerGeometry)){map = map + geom_sf(data = layerGeometry, 
+                                                fill= layerFill,
+                                                color = layerCol) }
+  return (ggiraph(code = print(map)))
+}
+displayMap()
