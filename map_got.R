0d66ca0a63a9cb6c73cec0c172d07c74be37ff81
kouadio jean philippe d'anzi <jko@iknova.com>
2020-11-08 10:37:33 -0100

    refactor and reoptimize code

--- map_got.R
+++ map_got.R
@@ -19,36 +19,15 @@ wall = st_read("data/GoTRelease/Wall.shp", crs = 4326)
 #' creation d'une dataframe contenant toutes les datas utiles.
 #' Pour pouvoir reconnaitre les differentes datas, ajout de l'attribut type
 
-#'\code{addTypeAttribut} ajoute à une dataframe une colonne de nom
-#'@param nameAttribut avec pour valeur
-#'@param valueAttribut dans la data
-#'@param dataName et @return la dataframe
-addTypeAttribut <-
-  function(dataName, valueAttribut, nameAttribut = "type") {
-    return (bind_cols(dataName, data.frame (nameAttribut = rep(
-      valueAttribut, length(dataName$id)
-    ))))
-  }
+continents$type="continent"
+islands$type="island"
+lakes$type="lake"
+rivers$type="river"
+roads$type="road"
+wall$type="wall"
 
 
-#addTypeAttribut(continents, "type", "continent")
-continentType = data.frame ("type" = rep("continent", length(continents$id)))
-islandType = data.frame("type" = rep("island", length(islands$id)))
-lakesType = data.frame("type" = rep("lakes", length(lakes$id)))
-riverType = data.frame("type" = rep("rivers", length(rivers$id)))
-roadsType = data.frame("type" = rep("roads", length(roads$id)))
-wallType = data.frame("type" = rep("wall", length(wall$id)))
-#policalType = data.frame("type" = rep("political", length(political$id)))
-
-allDatas = bind_rows(
-  bind_cols(continents, continentType),
-  bind_cols(islands, islandType),
-  landscape,
-  bind_cols(rivers, riverType),
-  bind_cols(lakes, lakesType),
-  bind_cols(roads, roadsType),
-  bind_cols(wall, wallType),
-)
+allDatas = bind_rows( continents,islands, landscape, rivers,lakes,roads,wall)
 
 spaces = c(
   "continent",
@@ -56,9 +35,9 @@ spaces = c(
   "mountain",
   "stepp",
   "swamp",
-  "lakes",
-  "rivers",
-  "roads",
+  "lake",
+  "river",
+  "road",
   "island",
   "location",
   "wall",
@@ -95,9 +74,9 @@ levels(allDatas$type) = spaces
 #'\code{displayMap} Consruire la map de got en fonction
 #'@param layerGeometry la couche donc la géometrie sera ajoutée et
 #'@param layerCol pour fixer la couleur de la couche
-#'@param layerFill pour fixer la couleur ndu remplissage
+#'@param layerFill pour fixer la couleur du remplissage
 #'@return une novelle map
-displayMap = function(layerGeometry=locations, layerCol="red", layerFill="red") {
+displayMap = function(layerGeometry=NA, layerCol="red", layerFill="red") {
   map = ggplot(allDatas) + geom_sf(aes(fill = type), size = 0.1) +
     geom_sf(data = locations,fill = "black",color = "black") +
     
@@ -110,7 +89,7 @@ displayMap = function(layerGeometry=locations, layerCol="red", layerFill="red") 
       color = "red",
       fontface = "bold"
     )
-  if(layerGeometry!= locations){map = map + geom_sf(data = layerGeometry, 
+  if(!is.na(layerGeometry)){map = map + geom_sf(data = layerGeometry, 
                                                 fill= layerFill,
                                                 color = layerCol) }
   return (ggiraph(code = print(map)))
